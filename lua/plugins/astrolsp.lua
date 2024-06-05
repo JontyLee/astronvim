bc8f04f0da50c2e9d72f62c03c300301c0a23054
JontyLee <ljlbj@foxmail.com>
2024-06-06 01:36:08 +0800

    modify

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -36,7 +36,7 @@ return {
       },
       disabled = { -- disable formatting capabilities for the listed language servers
         -- disable lua_ls formatting capability if you want to use StyLua to format your lua code
-        "intelephense",
+        "volar",
       },
       timeout_ms = 100000, -- default format timeout
       -- filter = function(client) -- fully override the default formatting function
